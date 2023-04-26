b7472667058f07f9c1c1f7a7e6d04e66ad7a17a7
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-04-26 15:28:28 +0530

    init project with vite+react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=''>Hello</div>
+    </>
+  )
+}
+
+export default App
