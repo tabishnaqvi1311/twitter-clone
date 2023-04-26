795c6619e12b6f90f913f779b854714dea8ee465
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-04-26 16:11:36 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SideNav from './components/SideNav'
+import Home from './pages/Home'
+import TweetDetail from './pages/TweetDetail'
+import Explore from './pages/Explore'
+import Notifications from './pages/Notifications'
+import Messages from './pages/Messages'
+import Bookmarks from './pages/Bookmarks'
+import Profile from './pages/Profile'
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className=''>Hello</div>
+      <BrowserRouter>
+        <SideNav/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/explore' element={<Explore/>}/>
+          <Route path='/notifications' element={<Notifications />}/>
+          <Route path='/messages' element={<Messages />}/>
+          <Route path='/bookmarks' element={<Bookmarks />}/>
+          <Route path='/:user' element={<Profile/>}/>
+          <Route path='/:user/status/:id' element={<TweetDetail/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/TweetDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TweetDetail = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TweetDetail
